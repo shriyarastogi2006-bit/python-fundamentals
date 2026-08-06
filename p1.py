08e5183ca04de9d7251e61a3cc19c53ea6e95421
shriyarastogi2006-bit <shriyarastogi2006@gmail.com>
2026-08-06 19:27:35 +0530

    commit

---
+++ p1.py
@@ -0,0 +1,91 @@
+print("hello world")
+print("This is a test script.")
+print("newyork","los angeles", "chicago", "houston", "phoenix")
+print(23)
+print(4+9)
+# name="shriya"
+# age=19
+# age2=age
+# sgpa=9.17
+# print(age2)
+# print("my name is",name,"and my age is",age)
+# print(name)
+# print(type(name))
+# print(type(age))
+# print(type(sgpa))
+old=True
+print(type(old))
+a=20
+b=34
+sum=a+b
+print("sum of:",a,"and",b,"is",sum)
+a=2
+b=3
+sum=a+b
+print(sum)
+mul=a*b
+print(mul)
+div=a/b
+print(div)  #alway sgives float value
+sub=a-b
+print(sub)
+mod=a%b
+print(mod)
+pow=a**b
+print(pow)
+
+#relational operators
+a=50
+b=20
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
+print(a<=b)
+print(a>=b)
+
+#assignment operators
+# num=10
+# num+=10
+# num*=10
+# num/=10
+# num**=10
+# num-=10
+# print("num:",num)
+
+#logical operators
+# a=20
+# b=10
+# print(not False)
+# print(not(a>b))
+# val1=True
+# val2=False
+# print("ans:",val1 and val2)
+# print("ans:",val1 or val2)
+# print((a==b) or (a>b))
+# a=int("2")
+# b=4.25
+# print(type(a))
+# print(a+b)
+# name=input("enter your name:")
+# print("welcome",name)
+# int("5")
+# val=float(input("enter some val:"))
+# print(type(val),val)
+# name=input("enter your name:")
+# age=int(input("enter your age:"))
+# marks=float(input("enter your marks:"))
+# print("name:",name)
+# print("age:",age)
+# print("marks:",marks)
+# a=int(input("enter first number:"))
+# b=int(input("enter second number:"))
+# print("sum:",a+b)
+s=int(input("enter any number:"))
+print("area:",s*s)
+l=float(input("enter first number:"))
+m=float(input("enter second number:"))
+print("avg:",(l+m)/2)
+y=int(input("enter first number:"))
+u=int(input("enter second number:"))
+print(a>=b)
