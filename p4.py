82f07c097ad209184ba6afc1004057985c0044ce
shriyarastogi2006-bit <shriyarastogi2006@gmail.com>
2026-08-10 13:15:02 +0530

    commit

---
+++ p4.py
@@ -0,0 +1,10 @@
+student= {
+    "name":"sadie",
+    "age":23,
+     "subjects":["maths","english","science"],
+     "tuple":(4,5,6,7,8),
+    "sgpa":9.7,
+    "is_student":True       
+}
+print(student)
+# key= string,number,tuple,value,float,list,boolean
